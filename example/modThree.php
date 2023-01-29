878cba8141d263ea22618190f4a1cfbef4860d89
Fujie Chen <imaple7@gmail.com>
2023-01-29 01:34:48 -0600

    update demo

--- example/modThree.php
+++ example/modThree.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 require '../vendor/autoload.php';
 
 use FSM\Alphabet\Letter;
+use FSM\FSM\FSMArrayBuilder;
+use FSM\FSM\FSMObjectBuilder;
 use FSM\FSMService;
 use FSM\State\FinalState;
 use FSM\Transition\Transition;
@@ -27,56 +29,80 @@ $state2 = new FinalState('S2', '2');
 $letter0 = new Letter('0');
 $letter1 = new Letter('1');
 
-$states = [$state0, $state1, $state2];
-$alphabet = [$letter0, $letter1];
-$initialState = $state0;
-$finalState = [$state0, $state1, $state2];
-$transitions = [
+$fsmArrayBuilder = new FSMArrayBuilder();
+$fsmArrayBuilder->createFSM();
+$fsmArrayBuilder->setStates([$state0, $state1, $state2]);
+$fsmArrayBuilder->setAlphabet([$letter0, $letter1]);
+$fsmArrayBuilder->setInitState($state0);
+$fsmArrayBuilder->setFinalState([$state0, $state1, $state2]);
+$fsmArrayBuilder->setTransitions([
     new Transition($state0, $letter0, $state0),
     new Transition($state0, $letter1, $state1),
     new Transition($state1, $letter0, $state2),
     new Transition($state1, $letter1, $state0),
     new Transition($state2, $letter0, $state1),
     new Transition($state2, $letter1, $state2),
-];
+]);
+$fsm = $fsmArrayBuilder->getFSM();
 
-$fsm = FSMService::buildFromArray($states, $alphabet, $initialState, $finalState, $transitions);
+$fsmService = new FSMService($fsm);
 
 // Example 1 - 110
 echo "Example 1 - 110:\n";
 
-$fsm->processList([
+$fsmService->processList([
     $letter1,
     $letter1,
     $letter0,
 ]);
 
-echo 'result: ';
-echo $fsm->getFinalOutput(). "\n";
+echo 'Result: ';
+echo $fsmService->getFinalOutput(). "\n\n";
 
 // Example 2 - 1010
 
 echo "Example 2 - 1010:\n";
 
-$fsm->reset();
+$fsmService->reset();
 
-$fsm->processList([
+$fsmService->processList([
     $letter1,
     $letter0,
     $letter1,
     $letter0,
 ]);
 
-echo 'result: ';
-echo $fsm->getFinalOutput(). "\n";
+echo 'Result: ';
+echo $fsmService->getFinalOutput(). "\n\n";
 
 // Example 3 - 1100 1100 1010 1101 1001 1011
 
+$fsmObjectBuilder = new FSMObjectBuilder();
+$fsmObjectBuilder->createFSM();
+$fsmObjectBuilder->addState($state0);
+$fsmObjectBuilder->addState($state1);
+$fsmObjectBuilder->addState($state2);
+$fsmObjectBuilder->addLetter($letter0);
+$fsmObjectBuilder->addLetter($letter1);
+$fsmObjectBuilder->setInitState($state0);
+$fsmObjectBuilder->addFinalState($state0);
+$fsmObjectBuilder->addFinalState($state1);
+$fsmObjectBuilder->addFinalState($state2);
+$fsmObjectBuilder->addTransition(new Transition($state0, $letter0, $state0));
+$fsmObjectBuilder->addTransition(new Transition($state0, $letter1, $state1));
+$fsmObjectBuilder->addTransition(new Transition($state1, $letter0, $state2));
+$fsmObjectBuilder->addTransition(new Transition($state1, $letter1, $state0));
+$fsmObjectBuilder->addTransition(new Transition($state2, $letter0, $state1));
+$fsmObjectBuilder->addTransition(new Transition($state2, $letter1, $state2));
+$fsm = $fsmObjectBuilder->getFSM();
+
+$fsmService = new FSMService($fsm);
+
 echo "Example 3 - 1100 1100 1010 1101 1001 1011:\n";
 
-$fsm->reset();
+$fsmService->reset();
 
-$fsm->processList([
+$fsmService->processList([
     $letter1,
     $letter1,
     $letter0,
@@ -103,5 +129,5 @@ $fsm->processList([
     $letter1,
 ]);
 
-echo 'result: ';
-echo $fsm->getFinalOutput(). "\n";
+echo 'Result: ';
+echo $fsmService->getFinalOutput(). "\n";
